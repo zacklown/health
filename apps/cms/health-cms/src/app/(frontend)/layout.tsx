090b0aff81480fb5b967def774f481417a625294
Zack L <35443539+zacklown@users.noreply.github.com>
2026-04-16 00:43:18 -1000

    Fixed Light mode issue and added in google analytics tags

--- apps/cms/health-cms/src/app/(frontend)/layout.tsx
+++ apps/cms/health-cms/src/app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './styles.css'
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -16,6 +17,21 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VYKY86LPDT"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VYKY86LPDT');
+          `}
+        </Script>
+      </head>
       <body>
         <main>{children}</main>
       </body>
